fb1a0cddf6d6f41ebe3f31b129643f9753b5b691
Matthijs Boers <matthijsje.boers@gmail.com>
2021-12-07 00:10:23 +0100

    Further cleanup of Rust folder

--- Rust/target/debug/deps/advent_of_code-80de5fc2754235ad.d
+++
@@ -1,7 +0,0 @@
-C:\Users\matth\Desktop\Advent Of Code 2021\Rust\target\debug\deps\advent_of_code-80de5fc2754235ad.rmeta: src\main.rs
-
-C:\Users\matth\Desktop\Advent Of Code 2021\Rust\target\debug\deps\advent_of_code-80de5fc2754235ad.d: src\main.rs
-
-src\main.rs:
-
-# env-dep:CLIPPY_ARGS=
